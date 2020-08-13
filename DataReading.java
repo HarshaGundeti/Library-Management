ee738e5ab25a9da7197fc62df801a615d856aa46
chsidhartha <53657955+chsidhartha@users.noreply.github.com>
2020-08-13 23:04:53 +0530

    Update DataReading.java

--- DataReading.java
+++ DataReading.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-
+package testrun;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,7 +23,7 @@ import java.util.*;
 //import com.google.common.collect.Table; 
 public class DataReading {
         String bookname;
-        File f,f2,f3,f4,f5;
+        File f,f2,f3,f4,f5,f6,f7,f8,f9;
         int counter;
         HashMap<String,ArrayList<String>> Atou=new HashMap<String,ArrayList<String>>();
         HashMap<String,String> Ntou=new HashMap<String,String>();
@@ -32,6 +32,10 @@ public class DataReading {
         HashMap<String,String> utobn=new HashMap<String,String>();
         HashMap<String,String> utog=new HashMap<String,String>();
         HashMap<String,String> utol=new HashMap<String,String>();
+        HashMap<String,String> itop=new HashMap<String,String>();
+        HashMap<String,String> itoo=new HashMap<String,String>();
+        HashMap<String,ArrayList<String>> itob=new HashMap<String,ArrayList<String>>();
+        HashMap<String,ArrayList<String>> btoi=new HashMap<String,ArrayList<String>>();
         DataReading(){
              bookname="";
              f= new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\booksname.txt");
@@ -39,6 +43,10 @@ public class DataReading {
              f3=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\booksnumber.txt");
              f4=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\booksgenre.txt");
              f5=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\bookslocation.txt");
+             f6=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\idPassword.txt");
+             f7=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\bookDate.txt");
+             f8=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\owe.txt");
+             f9=new File("C:\\Users\\chsra\\OneDrive\\Desktop\\LibraryManagement\\bookid.txt");
              counter=0;
         }
         public void bookReadAuthor()
@@ -49,10 +57,9 @@ public class DataReading {
                    String s=sc.nextLine();
                    String ucode=s.substring(0,6);
                    String Aname=s.substring(6);
-                   ArrayList<String> temp=Atou.get(Aname);
-                   temp.add(ucode);
-                   Atou.put(Aname, temp);
                    utoA.put(ucode,Aname);
+                   Atou.putIfAbsent(Aname,new ArrayList<String>());
+                   Atou.get(Aname).add(ucode);
                }
            }
            catch(Exception e)
@@ -130,7 +137,82 @@ public class DataReading {
                
            }
         }
-        
+        public void idPassword()
+        {
+           try{
+               Scanner sc=new Scanner(f6);
+               while(sc.hasNextLine()){
+                   String s=sc.nextLine();
+                   String id=s.substring(0,6);
+                   String password=s.substring(6);
+                   itop.put(id,password);
+               }
+           }
+           catch(Exception e)
+           {
+               
+           }
+        }
+         public void idOwe()
+        {
+           try{
+               Scanner sc=new Scanner(f8);
+               while(sc.hasNextLine()){
+                   String s=sc.nextLine();
+                   String id=s.substring(0,6);
+                   String owe=s.substring(6);
+                   itoo.put(id,owe);
+               }
+           }
+           catch(Exception e)
+           {
+               
+           }
+        }
+        public void booksAndDate()
+        {
+           try{
+               Scanner sc=new Scanner(f7);
+               while(sc.hasNextLine()){
+                   String s=sc.nextLine();
+                   String id=s.substring(0,6);
+                   int start=6;
+                   String datebook=s.substring(6);
+                   //ArrayList<String> al=new ArrayList<String>();
+                   itob.putIfAbsent(id,new ArrayList<String>());
+                   itob.get(id).add(datebook);
+               }
+           }
+           catch(Exception e)
+           {
+               
+           }
+        }
+        public void booksid()
+        {
+           try{
+               Scanner sc=new Scanner(f9);
+               while(sc.hasNextLine()){
+                   String s=sc.nextLine();
+                   String book=s.substring(0,6);
+                   int start=6;
+                   String id;
+                   ArrayList<String> al=new ArrayList<String>();
+                   while(start<s.length())
+                   {
+                       id=s.substring(start,start+6);
+                       al.add(id);
+                       start=start+6;
+                   }
+                   
+                   btoi.put(book,al);
+               }
+           }
+           catch(Exception e)
+           {
+               
+           }
+        }
         public int returncounter()
         {
             System.out.println("Hi!!!");
